8c0bef17357e697558342083c22631303d3c0785
memmickffin-pixel <memmickffin@gmail.com>
2026-01-27 19:46:12 +0000

    commit

---
+++ hover-amplify-column/myVisual/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var myVisual4304124F39F54F47B72071FECF0A051E_DEBUG: IVisualPlugin;
+export default myVisual4304124F39F54F47B72071FECF0A051E_DEBUG;
